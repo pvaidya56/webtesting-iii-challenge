99bd2fa727eb5c7df3a3351e030788d3938f86d6
Priya <pvaidya56@gmail.com>
2019-12-08 03:04:40 -0800

    wrote tests for mvp

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -11,3 +11,37 @@ test('renders without crashing', () => {
 test('should match snapshot', () => {
     expect(render(<Dashboard />)).toMatchSnapshot();
 })
+
+//shows controls and display
+
+test ('default state is unlocked and open', ()=> {
+    const { getByText } = render(<Dashboard />);
+    getByText(/unlocked/i);
+    getByText(/open/i);
+  
+    const lockBtn = getByText(/lock gate/i);
+    const closeBtn = getByText(/close gate/i);
+  
+    expect(lockBtn.disabled).toBe(true);
+    expect(closeBtn.disabled).toBe(false);
+  })
+
+  test('cannot be closed or opened if it it locked', ()=> {
+    const { getByText } = render (<Dashboard />);
+    
+    const lockBtn = getByText(/lock gate/i);
+    const closeBtn = getByText(/close gate/i);
+    
+    fireEvent.click(closeBtn);
+    fireEvent.click(lockBtn);
+  
+    getByText(/locked/i)
+    getByText(/closed/i);
+  
+    const openBtn = getByText(/open gate/i);
+    const unlockBtn = getByText(/unlock gate/i);
+  
+    expect(openBtn).toBeTruthy();
+    expect(openBtn.disabled).toBe(true);
+    expect(unlockBtn.disabled).toBe(false);
+  })
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -12,3 +12,41 @@ test('renders without crashing', () => {
 test('should match snapshot', () => {
     expect(render(<Display />)).toMatchSnapshot();
 })
+
+//test unlocked and open
+test ('unlocked and open', ()=>{
+  const { getByText } = render(<Display />)
+
+  const open = getByText(/open/i);
+  const unlocked = getByText(/unlocked/i);
+
+  expect(open.className).toMatch(/green-led/i);
+  expect(unlocked.className).toMatch(/green-led/i);
+
+})
+
+//test unlocked and closed
+test('unlocked and closed', ()=>{
+  const { getByText } = render(<Display closed={true} locked={false}/>)
+
+  const close = getByText(/closed/i);
+  const unlock = getByText(/unlocked/i);
+
+  expect(close.className).toMatch(/red-led/i);
+  expect(unlock.className).toMatch(/green-led/i);
+})
+
+//test locked and closed
+test('locked and closed', ()=>{
+  const { getByText } = render(<Display closed={true} locked={true}/>)
+
+  const close = getByText(/closed/i);
+  const lock = getByText(/locked/i);
+
+  expect(close.className).toMatch(/red-led/i);
+  expect(lock.className).toMatch(/red-led/i);
+})
+
+
+
+
